e53f28f4be30bdc6fdc9df4228f54e31ede1aba5
BobAnkh <bobankhshen@gmail.com>
2020-11-02 17:00:28 +0800

    chore(test): modify path

--- code/demo.py
+++ code/demo.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 from rendering import rendering
 
-dirname = 'dataset\dataset_offline'
+dirname = os.path.join('dataset', 'dataset_offline')
 dirs = os.listdir(dirname)
 for dir in dirs:
     if dir[0] == 'P':
